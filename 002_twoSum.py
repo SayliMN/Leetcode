c90c2efdf2ccdb089f93f57eb5719fd0d91ddd45
SayliMN <35377449+SayliMN@users.noreply.github.com>
2021-05-03 17:18:21 -0400

    Update 002_twoSum.py

--- 002_twoSum.py
+++ 002_twoSum.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Com plexity: O(n^2)
+        # Brute force
         n = len(nums)
         for i in range(n-1):
             for j in range(i+1,n):
                 sum_1 = nums[i] + nums[j]
                 if sum_1 == target:
                     return i, j 
+                
+        
