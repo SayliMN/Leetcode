f4058605526c63866b997d83ac55ab4d9a608c8c
SayliMN <35377449+SayliMN@users.noreply.github.com>
2021-07-14 11:01:07 -0400

    Create 003_Reverse_Integer.py

---
+++ 003_Reverse_Integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        min_val = -1 * 2**31
+        max_val = 2**31 - 1
+        if x >= 0:
+            a = int(str(x)[::-1])
+        if x < 0:
+            a = -1 * int(str(x*-1)[::-1])
+        
+        if a not in range(min_val, max_val):
+            return 0
+        else:
+            return a
