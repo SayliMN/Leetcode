69f07e94e974cc0149d61e724c77d29232185d68
SayliMN <35377449+SayliMN@users.noreply.github.com>
2022-01-25 16:09:40 -0800

    Create 042_trap.py

---
+++ 042_trap.py
@@ -0,0 +1,23 @@
+class Solution:
+# Time complexity: O(n)
+# Space complexity: O(1)
+  
+    def trap(self, height: List[int]) -> int:
+        max_h_left, max_h_right, water = 0, 0, 0
+        max_h = 0
+        max_h_ind = 0
+        
+        for i in range(len(height)):
+            if height[i] > max_h:
+                max_h = max(height[i],max_h)
+                max_h_ind = i
+        
+        for j in range(max_h_ind):
+            max_h_left = max(height[j], max_h_left)
+            water += max_h_left - height[j]
+            
+        for l in range(len(height)-1, max_h_ind,-1):
+            max_h_right = max(height[l], max_h_right)
+            water += max_h_right - height[l]
+        
+        return water
