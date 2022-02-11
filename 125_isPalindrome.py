ff4fdaccb69c8b59982ad649ccef72092f5ff717
SayliMN <35377449+SayliMN@users.noreply.github.com>
2022-02-11 09:58:34 -0800

    Create 125_isPalindrome.py

---
+++ 125_isPalindrome.py
@@ -0,0 +1,18 @@
+l = 0
+        r = len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+        
+#         Time complexity: O(n)
+#         I traverse over each character at-most once, until l and right pointers meet in the middle.
+        
+#         Space complexity: O(1)
+#         No extra space is required
