a5d5776b83c3347ff3bfdec0e4594769453c69be
SayliMN <35377449+SayliMN@users.noreply.github.com>
2022-02-10 17:28:08 -0800

    Create 520_detectCapitalUse.py

---
+++ 520_detectCapitalUse.py
@@ -0,0 +1,20 @@
+n = len(word)
+        
+        if len(word) == 1:
+            return True
+        
+        if word[0].isupper() and word[1].isupper():
+            for i in range(2,n):
+                if not word[i].isupper():
+                    return False
+        else:
+            for i in range(1,n):
+                if word[i].isupper():
+                    return False
+        return True
+        
+#         Time Complexity: O(n) 
+#         Checking chars by chars in the length of word
+        
+#         Space Complexity: O(1)
+#         Using constant spaces to store variables
