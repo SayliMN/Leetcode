7e116a213191276b4b7ebc866d6bcc15bc310316
SayliMN <35377449+SayliMN@users.noreply.github.com>
2022-02-15 17:01:24 -0800

    Create 242_isAnagram.py

---
+++ 242_isAnagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return collections.Counter(s) == collections.Counter(t)
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+        
+        dictionary = {}
+        for i in s:
+            if i in dictionary:
+                dictionary[i] += 1
+            else:
+                dictionary[i] = 1
+                
+        for j in t:
+            if j in dictionary: 
+                dictionary[j] -= 1
+            else:
+                return False
+            
+        for val in dictionary.values():
+            if val != 0:
+                return False
+            
+        return True
