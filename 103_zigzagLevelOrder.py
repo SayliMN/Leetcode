fc60e20176c343f12b1717cd2f2fa284d137f72b
SayliMN <35377449+SayliMN@users.noreply.github.com>
2022-04-02 00:50:45 -0700

    Update 103_zigzagLevelOrder.py

--- 103_zigzagLevelOrder.py
+++ 103_zigzagLevelOrder.py
@@ -4,22 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
         
         queue = collections.deque([root])
         result = []
+        going_right = True
         while queue:
             level = []
             for i in range(len(queue)):
-                node = queue.popleft()
-                if node:
+                if going_right:
+                    node = queue.popleft()
                     level.append(node.val)
-                    queue.append(node.left)
-                    queue.append(node.right)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+                else:
+                    node = queue.pop()
+                    level.append(node.val)
+                    if node.right:
+                        queue.appendleft(node.right)
+                    if node.left:
+                        queue.appendleft(node.left)
             if level:
                 result.append(level)
+            going_right = not going_right
         return result 
       
 #       Time: O(N)
