d0d7dda1fd56feb26ad2ef9aada96d4241f1dbbb
SayliMN <35377449+SayliMN@users.noreply.github.com>
2022-05-30 21:43:28 -0700

    Create 70_climbStairs.py

---
+++ 70_climbStairs.py
@@ -0,0 +1,33 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if 0 < n <= 2:
+            return n
+        
+        first = 1
+        second = 2
+        third = 0
+        for i in range(3, n+1):
+            third = first + second
+            first = second
+            second = third
+        return second
+    
+    # O(n),O(1)
+        
+#         if n <= 2:
+#             return n
+        
+#         dp = [0]*(n+1)
+#         dp[1], dp[2] = 1,2
+#         for i in range(3,n+1):
+#             dp[i] = dp[i-1] + dp[i-2]
+#         return dp[i]
+#     # O(n),O(n)
+            
+        # a = b = 1
+        # for _ in range(n):
+        #     a, b = b, a + b
+        # return a
+        
+       
+            
