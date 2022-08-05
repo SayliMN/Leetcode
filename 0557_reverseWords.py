45d0b09683f7e5e2c002286dac93251611688125
SayliMN <35377449+SayliMN@users.noreply.github.com>
2022-08-05 14:41:23 -0700

    Update 0557_reverseWords.py

--- 0557_reverseWords.py
+++ 0557_reverseWords.py
@@ -6,6 +6,34 @@
         
         return ' '.join(x[::-1] for x in s.split())
     
-        # Time Complexity: O(n)
-        # Space Complexity: O()
+        # Time Complexity: O(n*2)
+        # Space Complexity: O(1)
+  
+
+ # Approach 2: Sliding window
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+  def reverseWords(self, s: str) -> str:      
+     result = ""
+     chars = 0
+     for i, char in enumerate(s):
+        if char != " ":
+           chars += 1
+        else:
+           result += " " if result else ""
+           k = i - 1
+           while(chars):
+              result += s[k]
+              chars -= 1
+              k -= 1
+     result += " " if result else ""   
+     while(chars):
+        result += s[i]
+        chars -= 1
+        i -= 1
+     return result
+          
+             
         
