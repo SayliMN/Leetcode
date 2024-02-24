cfe73bf07c94cc1ccace12d344f1ff33a1ab6c37
Sayli Narkhede <35377449+SayliMN@users.noreply.github.com>
2024-02-23 23:53:19 -0800

    update 0973_kClosest.py

--- 0973_kClosest.py
+++ 0973_kClosest.py
@@ -1,8 +1,5 @@
 class Solution:
     def kClosest(self, points, k):
-        # return sorted(points, key=lambda p: p[0]**2 + p[1]**2)[0:k]
-        # time complexity: O(nlogn)
-        # space complexity: O(n)
         
         heap, euclidean = [], lambda x, y : x*x + y*y
         for i, (x, y) in enumerate(points):
@@ -12,5 +9,6 @@ class Solution:
             else: 
                 heappush(heap, (-d, i))
         return [points[i] for (_, i) in heap]
+        
         # time complexity: O(nlogk)
         # space complexity: O(k)
