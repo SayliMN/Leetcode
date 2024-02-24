045dbcfeb91bcd607dceba18666f247223056145
Sayli Narkhede <35377449+SayliMN@users.noreply.github.com>
2024-02-23 23:52:12 -0800

    update 0001_twoSum.py

--- 0001_twoSum.py
+++ 0001_twoSum.py
@@ -1,13 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Complexity: O(n^2)
-        # Brute force
-#         n = len(nums)
-#         for i in range(n-1):
-#             for j in range(i+1,n):
-#                 sum_1 = nums[i] + nums[j]
-#                 if sum_1 == target:
-#                     return i, j 
 
         # Complexity:O(n)
         # Found index of element's complement
@@ -23,4 +15,4 @@ class Solution:
                 return (dictionary[comp],i)
             dictionary[nums[i]] = i
 
-        # compleexity: O(n)
+        # complexity: O(n)
