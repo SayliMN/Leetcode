7c2d7cfed4415aa8b0070972f5126068563d1349
Sayli Narkhede <35377449+SayliMN@users.noreply.github.com>
2024-08-08 23:37:57 +0530

    update 0001_twoSum.py

--- 0001_twoSum.py
+++ 0001_twoSum.py
@@ -1,13 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        # Complexity:O(n)
-        # Found index of element's complement
-#         for i, e in enumerate(nums):
-#             diff = target - e
-#             if (diff in nums) and (i !=nums.index(diff)) :
-#                 return [nums.index(diff), i]
-           
+    def twoSum(self, nums: List[int], target: int) -> List[int]:           
         dictionary = {}
         for i, v in enumerate(nums):
             comp = target - nums[i]
